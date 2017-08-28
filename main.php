85f7088733700a8a52ec1359e4f7765054b0bf6d
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-08-28 07:02:24 +0700

    add source

---
+++ main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Roux Academy App</title>
+	<link rel="stylesheet" type="text/css" href="/styles/main.css">
+</head>
+<body>
+	<div id="outerWrapper">
+		<header>
+			<img src="/images/ralogo_monogram.png" width="109" height="63" alt="Roux Academy">
+			<p id="raTitle">Roux Academy <br>
+				Art &bull; Media &bull; Design
+			</p>
+		</header>
+		<article id="main">
+			<?php echo "<h1>App lauched!</h1>" ?>
+		</article>
+	</div>
+</body>
+</html>
